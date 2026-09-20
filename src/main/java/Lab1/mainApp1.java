ff76601731a6f7d44c2acc3dfe7fad4b3d360077
Fandy Chen <kennyassssss1@gmail.com>
2026-09-20 17:07:29 +0800

    Add Lab1 main application

---
+++ src/main/java/Lab1/mainApp1.java
@@ -0,0 +1,13 @@
+package Lab1;
+
+/** Entry point for the COMP3111 Lab 1 demonstration. */
+public class mainApp1 {
+
+    public static void main(String[] args) {
+        System.out.println("COMP3111 Lab 1 - Git and GitHub");
+        System.out.println("Demonstrating the myLibrary class");
+        System.out.println("---------------------------------");
+        System.out.println("2 raised to the power of 10 = " + myLibrary.power(2, 10));
+        System.out.println("The factorial of 5 = " + myLibrary.factorial(5));
+    }
+}
